18436f0b12578c68e639be812cfe11baab654fc8
Philip <ajibolaphilip10@gmail.com>
2024-03-15 01:13:31 -0700

    commit

--- test/data/repositories/EntryRepositoriesImplementTest.java
+++ test/data/repositories/EntryRepositoriesImplementTest.java
@@ -1,7 +1,13 @@
 package data.repositories;
 
+import org.junit.jupiter.api.BeforeEach;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class EntryRepositoriesImplementTest {
-
+    private EntryRepositoriesImplement entryRepositoriesImplement;
+    @BeforeEach
+    public void createEntry(){
+        entryRepositoriesImplement = new EntryRepositoriesImplement();
+    }
 }
