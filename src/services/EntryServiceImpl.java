580a61fe298d686e46e5f99ff8253ad777ff465f
Philip <ajibolaphilip10@gmail.com>
2024-03-18 13:40:38 -0700

    commit

--- src/services/EntryServiceImpl.java
+++ src/services/EntryServiceImpl.java
@@ -1,11 +1,14 @@
 package services;
 
 import data.model.Entry;
+import data.repositories.EntryRepositories;
+import data.repositories.EntryRepositoriesImplement;
 
 public class EntryServiceImpl implements EntryService{
+    private EntryRepositories entryRepositories = new EntryRepositoriesImplement();
     @Override
     public void create(Entry entry) {
-
+        entryRepositories.save(entry);
     }
     @Override
     public void update(Entry entry) {
