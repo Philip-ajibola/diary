706a50862477479c934445bb4cf47b32fcf6de0e
Philip <ajibolaphilip10@gmail.com>
2024-03-18 13:51:01 -0700

    commit

--- src/services/EntryService.java
+++ src/services/EntryService.java
@@ -1,10 +1,10 @@
 package services;
 
+import data.model.Diary;
 import data.model.Entry;
 
 public interface EntryService {
     void create(Entry entry);
     void update(Entry entry);
-    void delete(Entry entry);
-
+    void deleteEntry(String Author);
 }
