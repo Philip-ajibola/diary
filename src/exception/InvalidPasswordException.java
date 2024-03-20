666b623fffaab75f78deda8a505fd1358a5afb6c
Philip <ajibolaphilip10@gmail.com>
2024-03-19 23:34:43 -0700

    commit

--- src/exception/InvalidPasswordException.java
+++ src/exception/InvalidPasswordException.java
@@ -1,6 +1,6 @@
 package exception;
 
-public class InvalidPasswordException extends RuntimeException{
+public class InvalidPasswordException extends DiaryException{
     public InvalidPasswordException(String message){
         super(message);
     }
