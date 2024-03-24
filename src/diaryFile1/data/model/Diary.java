93cec173266e6530d9293dadf73b124a1e70df2c
Philip <ajibolaphilip10@gmail.com>
2024-03-23 21:12:09 -0700

    commit

---
+++ src/diaryFile1/data/model/Diary.java
@@ -0,0 +1,16 @@
+package diaryFile1.data.model;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Document
+public class Diary {
+    @Id
+    private String username;
+    private String password;
+    private boolean isLocked = true;
+
+
+    }
