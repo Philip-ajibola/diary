d644c773018e49587f747b7456e4cd1cce401d00
Philip <ajibolaphilip10@gmail.com>
2024-03-23 21:26:17 -0700

    commit

---
+++ src/diaryFile1/data/model/Entry.java
@@ -0,0 +1,30 @@
+package diaryFile1.data.model;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDate;
+@Data
+@Document
+public class Entry {
+
+    private  String author;
+    private  String title;
+    private  String body;
+    @Id
+    private String id;
+    private LocalDate localDate = LocalDate.now();
+
+    @Override
+    public String toString() {
+        return String.format("""
+                Entry Author: %s                   Date Of Creation: %s
+                
+                            Entry Title: %s
+                            
+                   Entry Body:
+                         %s         
+                """,author,localDate,title,body);
+    }
+}
