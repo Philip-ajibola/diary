6f7504a267315361d4a34afe4a023a35a388079f
Philip <ajibolaphilip10@gmail.com>
2024-03-23 21:34:18 -0700

    commit

--- src/diaryFile1/services/EntryServiceImpl.java
+++ src/diaryFile1/services/EntryServiceImpl.java
@@ -1,26 +1,29 @@
-package services;
+package diaryFile1.services;
 
-import data.model.Diary;
-import data.model.Entry;
-import data.repositories.EntryRepositories;
-import data.repositories.EntryRepositoriesImplement;
+import diaryFile1.data.model.Entry;
+import diaryFile1.data.repositories.EntryRepositories;
+import diaryFile1.exception.EntryNotFoundException;
+import diaryFile1.exception.UserNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
+@Service
 public class EntryServiceImpl implements EntryService{
-    private static EntryRepositories entryRepositories = new EntryRepositoriesImplement();
+    @Autowired
+    private   EntryRepositories entryRepositories ;
     @Override
     public void create(Entry entry) {
         entryRepositories.save(entry);
     }
     @Override
     public void deleteEntry(String author,String title) {
-       List<Entry> entries = findEntryOf(author);
-        Entry expected = null;
-        entries.forEach(entry->{ if(entry.getTitle().equals(title)){
-            entryRepositories.delete(entry);
-       }});
+        Entry entry = entryRepositories.findEntryByTitle(title);
+        if(!entryRepositories.existsById(entry.getId()))throw new EntryNotFoundException("Entry not found");
+        entryRepositories.deleteById(entry.getId());
     }
 
     @Override
